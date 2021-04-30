3e5841588d19eb30e6dea7c5368733a7991a26ad
章革 <1296327946@qq.com>
2021-04-30 09:26:09 +0800

    提交

--- fxs/src/router/index.js
+++ fxs/src/router/index.js
@@ -20,6 +20,11 @@ const routes = [
     name: 'zhangge',
     component: () => import('../views/zhangge.vue'),
   },
+  {
+    path: '/day01/index',
+    name: 'day01/index',
+    component: () => import('../views/day01/index'),
+  },
 ]
 
 const router = new VueRouter({
